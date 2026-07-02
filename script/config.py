3d91ff57ed4d3b74bdc0578efb8c471c6a9a5df7
vinawao <69920630+vinawao@users.noreply.github.com>
2026-07-02 09:24:41 +0800

    Create config.py

---
+++ script/config.py
@@ -0,0 +1,25 @@
+PLAYLIST_DIR = "playlists"
+
+OUTPUT_DIR = "output"
+
+ONLINE_PLAYLIST = "events_online.m3u"
+
+OFFLINE_PLAYLIST = "offline.m3u"
+
+MERGED_PLAYLIST = "merged.m3u"
+
+TIMEOUT = 8
+
+MAX_WORKERS = 32
+
+RETRIES = 2
+
+USER_AGENT = (
+    "Mozilla/5.0 "
+    "(Windows NT 10.0; Win64; x64)"
+)
+
+HEADERS = {
+    "User-Agent": USER_AGENT,
+    "Accept": "*/*"
+}
